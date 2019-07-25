5523746bdcf580ba2a03b671e610b41b35ba0343
Vitor Claudio Chaves de Aguiar <vaguiar@google.com>
2019-07-24 21:08:49 -0300

    Adding update and delete requests for category

--- src/components/FavoriteThingsDataTable.vue
+++ src/components/FavoriteThingsDataTable.vue
@@ -76,7 +76,7 @@
       </v-dialog>
       <v-dialog v-model="categoryDialog" max-width="500px">
         <template v-slot:activator="{ on }">
-          <v-btn dark color="#0191A9" class="mb-2" v-on="on">New Category</v-btn>
+          <v-btn @click="getCategories" dark color="#0191A9" class="mb-2" v-on="on">Manage Category</v-btn>
         </template>
         <v-card>
           <div v-if="inCategoryDialogProgress">
@@ -91,14 +91,40 @@
             </v-progress-linear>
           </div>
           <v-card-title>
-            <span class="headline">New Category</span>
+            <span class="headline">Manage Category</span>
           </v-card-title>
 
           <v-card-text>
             <v-container grid-list-md>
               <v-layout column>
                 <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="category.name" label="Name" :rules="basicRules" autofocus></v-text-field>
+                  <v-radio-group v-model="radioGroup" row @change="getButtonMessage">
+                    <v-radio
+                      label="New"
+                      value="1"
+                    ></v-radio>
+                    <v-radio
+                      label="Update"
+                      value="2"
+                    ></v-radio>
+                    <v-radio
+                      label="Delete"
+                      value="3"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-select
+                    v-model="editedItem.category"
+                    v-if="radioGroup > 1"
+                    :items="categories"
+                    item-text="name"
+                    item-value="id"
+                    label="Select a category">
+                  </v-select>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field v-if="radioGroup != 3" v-model="category.name" label="Name" autofocus></v-text-field>
                 </v-flex>
               </v-layout>
             </v-container>
@@ -106,7 +132,7 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="blue darken-1" flat @click="closeCategoryDialog">Cancel</v-btn>
-            <v-btn color="blue darken-1" flat @click="saveCategory">Save</v-btn>
+            <v-btn color="blue darken-1" flat @click="categoryAction">{{radioGroupMessage}}</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -151,6 +177,8 @@ import axios from 'axios';
 
 export default {
   data: () => ({
+    radioGroup: 1,
+    radioGroupMessage: 'Save',
     favoriteThingDialog: false,
     categoryDialog: false,
     snackbar: false,
@@ -268,6 +296,37 @@ export default {
       });
     },
 
+    updateFavoriteThing() {
+      this.setFavoriteDialogProgress(true, "Updating Favorite Thing...");
+      axios({
+        method: 'put',
+        url: '/api/favoritething/' + this.editedItem.id + '/',
+        headers: {'Content-Type': 'application/json'},
+        data: {
+          title: this.editedItem.title,
+          description: this.editedItem.description,
+          ranking: this.editedItem.ranking,
+          category_id: this.editedItem.category,
+          audit_log: "Loging",
+        }
+      })
+      .then(response => {
+        this.setFavoriteDialogProgress(false, "");
+        this.setSnackbar(true, "Successfuly updated the favorite thing " + response.data.title);
+        this.editedItem.title = null;
+        this.editedItem.description = null;
+        this.editedItem.ranking = null;
+        this.editedItem.category = null;
+        this.editedItem.metadata = null;
+        this.getFavoriteThings();
+      })
+      .catch(error => {
+        this.setFavoriteDialogProgress(false, "");
+        this.setSnackbar(true, "Failed while updating the favorite thing");
+        console.log(error);
+      });
+    },
+
     getCategories() {
       this.setFavoriteDialogProgress(true, "Loading Categories...");
       axios.get('/api/category')
@@ -300,6 +359,52 @@ export default {
       .catch(error => {
         this.setCategoryDialogProgress(false, "");
         this.setSnackbar(true, "Failed while creating the category");
+        console.log(error);
+      });
+    },
+
+    updateCategory() {
+      this.setCategoryDialogProgress(true, "Updating Category...");
+      axios({
+        method: 'put',
+        url: '/api/category/' + this.editItem.category.id + '/',
+        headers: {'Content-Type': 'application/json'},
+        data: {
+          name: this.category.name,
+        }
+      })
+      .then(response => {
+        this.setCategoryDialogProgress(false, "");
+        this.setSnackbar(true, "Successfuly updated the category " + response.data.name);
+        this.category.name = null;
+        console.log(response);
+      })
+      .catch(error => {
+        this.setCategoryDialogProgress(false, "");
+        this.setSnackbar(true, "Failed while updating the category");
+        console.log(error);
+      });
+    },
+
+    deleteCategory() {
+      this.setCategoryDialogProgress(true, "Deleting Category...");
+      axios({
+        method: 'delete',
+        url: '/api/category/' + this.editItem.category.id + '/',
+        headers: {'Content-Type': 'application/json'},
+        data: {
+          name: this.category.name,
+        }
+      })
+      .then(response => {
+        this.setCategoryDialogProgress(false, "");
+        this.setSnackbar(true, "Successfuly deleted the category " + response.data.name);
+        this.category.name = null;
+        console.log(response);
+      })
+      .catch(error => {
+        this.setCategoryDialogProgress(false, "");
+        this.setSnackbar(true, "Failed while deleting the category");
         console.log(error);
       });
     },
@@ -355,10 +460,27 @@ export default {
 
     closeCategoryDialog() {
       this.categoryDialog = false;
+    },
+
+    getButtonMessage() {
+      if (this.radioGroup == 1) { 
+        this.radioGroupMessage = 'Save';
+      } else if (this.radioGroup == 2) {
+        this.radioGroupMessage = 'Update';
+      } else if (this.radioGroup == 3) {
+        this.radioGroupMessage = 'Delete';
+      }
+    },
+
+    categoryAction() {
+      if (this.radioGroup == 1) {
+        this.saveCategory();
+      } else if(this.radioGroup == 2) {
+        this.updateCategory();
+      } else if(this.radioGroup == 3) {
+        this.deleteCategory();
+      }
     }
   }
 };
 </script>
-
-
-    
