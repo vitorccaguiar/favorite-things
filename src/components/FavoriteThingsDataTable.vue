d7073a54bed9f7392ce567e4e440432ed9bccf27
Vitor Claudio Chaves de Aguiar <vitor.claudio.aguiar@gmail.com>
2019-07-25 20:27:49 -0300

    Adding style for the metadata button

--- src/components/FavoriteThingsDataTable.vue
+++ src/components/FavoriteThingsDataTable.vue
@@ -142,7 +142,7 @@
         <td>{{ props.item.title }}</td>
         <td class="text-xs-left">{{ props.item.description }}</td>
         <td class="text-xs-left">{{ props.item.ranking }}</td>
-        <td class="text-xs-left"><v-btn style="margin: 0px;" color="primary">View</v-btn></td>
+        <td class="text-xs-left"><v-btn class="metadataButton" color="primary">View</v-btn></td>
         <td class="text-xs-left">{{ props.item.category.name }}</td>
         <td class="text-xs-left">{{ props.item.created_date }}</td>
         <td class="text-xs-left">{{ props.item.modified_date }}</td>
@@ -578,3 +578,9 @@ export default {
   }
 };
 </script>
+<style>
+  .metadataButton {
+    margin: 0px;
+    padding: 8px;
+  }
+</style>
