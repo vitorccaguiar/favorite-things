b2f5f29f0733c5417b4fffe05652aa8d64b94071
Vitor Claudio Chaves de Aguiar <vitor.claudio.aguiar@gmail.com>
2019-08-08 22:01:58 -0300

    Updating put audit log

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -21,7 +21,7 @@ class FavoriteThingSerializer(serializers.ModelSerializer):
         return obj
 
     def update(self, instance, validated_data):
-        FavoriteThing.objects.update(instance, **validated_data)
+        FavoriteThing.objects.update(instance)
         AuditLog.objects.create(action_performed='PUT', favorite_thing=instance)
         return instance
 
